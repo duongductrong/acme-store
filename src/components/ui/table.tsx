84cf6277183744a00cde31769aeac0cd3a864a5c
Dương Đức Trọng <duongductrong06@gmail.com>
2023-07-09 19:11:14 +0700

    chore: add background for header on table

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -20,7 +20,11 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
+  <thead
+    ref={ref}
+    className={cn("[&_tr]:border-b [&_tr]:bg-muted/50", className)}
+    {...props}
+  />
 ))
 TableHeader.displayName = "TableHeader"
 
